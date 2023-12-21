64e6a1e3d74140b1baf7c3cdd67d5eecb34ae5d7
FilipeHaniel <filipe.h.martins@timwe.com>
2023-12-20 22:53:10 -0300

    first api commit

---
+++ src/main/java/br/com/springboot/diet_pdf_creator_api/DietPdfCreatorApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.springboot.diet_pdf_creator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietPdfCreatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietPdfCreatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springboot/diet_pdf_creator_api/DietPdfCreatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springboot.diet_pdf_creator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietPdfCreatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
