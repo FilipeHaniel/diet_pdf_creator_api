edad5f50e7b5d923302f0350a7b0b13a9ae46bed
FilipeHaniel <filipe.h.martins@timwe.com>
2023-12-22 16:46:59 -0300

    return user

---
+++ src/main/java/br/com/springboot/diet_pdf_creator_api/models/User.java
@@ -0,0 +1,16 @@
+package br.com.springboot.diet_pdf_creator_api.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Long id;
+    private String name;
+    private String userName;
+}
